d08c1d8779452286459342705e82c2c53da805f3
alerodrez <ale.designspace@gmail.com>
2024-05-28 21:02:38 -0600

    Css finales

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,3 +6,16 @@ function closeNav(){
     document.getElementById("menu-mobile").style.width = "0%";
 }
 
+const target = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
+
+target.forEach(target => {
+	target.addEventListener('click', () => {
+		content.forEach(c => {
+			c.classList.remove('active')
+		})
+		const t = document.querySelector(target.dataset.target)
+		t.classList.add('active')
+	})
+})
+
